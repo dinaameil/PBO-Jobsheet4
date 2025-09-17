9cf844b2ae8c743c8ed63f7b8e55531cb63c4d9d
dinaameil <dinaameil753@gmail.com>
2025-09-17 21:36:56 +0700

    Praktikum relasi kelas

---
+++ Pegawai.java
@@ -0,0 +1,9 @@
+
+public class Pegawai {
+
+    public String getInfo() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getInfo'");
+    }
+
+}
